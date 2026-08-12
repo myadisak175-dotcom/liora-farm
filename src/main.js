c618bcfedf0d7460eff35eee3a604adbcf036e67
myadisak175-dotcom <myadisak175@gmail.com>
2026-08-13 01:34:00 +0700

    Bust cache for soft ground feather

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { CONFIG, ASSETS, ANIMATIONS } from "./config.js?v=dirt-ground1";
 import { createPlayer } from "./entities/player.js";
-import { createHomeIsland } from "./zones/home-island.js?v=dirt-ground1";
+import { createHomeIsland } from "./zones/home-island.js?v=soft-feather1";
 import { createInput } from "./systems/input.js";
 import { createMovementSystem } from "./systems/movement.js";
 import { createCameraController } from "./systems/camera.js";
