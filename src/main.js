9f02712dc46e20a1a07f8667c4592f8307f98a10
myadisak175-dotcom <myadisak175@gmail.com>
2026-08-13 00:19:00 +0700

    Refresh dirt texture and ground shader modules

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { CONFIG, ASSETS, ANIMATIONS } from "./config.js?v=dirt1";
+import { CONFIG, ASSETS, ANIMATIONS } from "./config.js?v=dirt3";
 import { createPlayer } from "./entities/player.js";
-import { createHomeIsland } from "./zones/home-island.js?v=dirt2";
+import { createHomeIsland } from "./zones/home-island.js?v=dirt3";
 import { createInput } from "./systems/input.js";
 import { createMovementSystem } from "./systems/movement.js";
 import { createCameraController } from "./systems/camera.js";
