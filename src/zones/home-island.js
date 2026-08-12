9663ce0a8b4c23dcd887986bac4948eff092f7f3
myadisak175-dotcom <myadisak175@gmail.com>
2026-08-13 00:18:24 +0700

    Bust ground paint shader cache

--- src/zones/home-island.js
+++ src/zones/home-island.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { createFloatingIsland } from "../systems/floating-island.js";
 import { createTerrain } from "../systems/terrain.js";
-import { createGroundPaint } from "../systems/ground-paint.js?v=dirt2";
+import { createGroundPaint } from "../systems/ground-paint.js?v=dirt3";
 import { createFarmPlot } from "../systems/farming/plot.js";
 
 function tile(texture, repeat) {
